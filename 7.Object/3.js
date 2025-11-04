6e0d4cbff708ad7dff06621df3f27f47ee232374
Eli <laamdun@gmai.com>
2025-11-04 16:58:38 +0700

    const or let

--- 7.Object/3.js
+++ 7.Object/3.js
@@ -1,16 +1,27 @@
 // Tại sao bạn nên sử dụng 'const' thay vì 'let' cho đối tượng trong JavaScript
-// 1. let cho phép tái định nghĩa, còn const thì không
+// Với khai báo thường
+var ar = 4;
+console.log(ar);
 
-let a = 5;
-console.log(a);
-a = 9;
-console.log(a);
+var ar = 5; //     có thể tái định nghĩa => rất dễ bị trùng
+console.log(ar);
+
+let ab = 3;
+console.log(ab);
+
+// let ab = 5;      bị lỗi khi tái định nghĩa
+// console.log(ab);
+
+ab = 5; //          nhưng có thể thay đổi giá trị ==> rủi ro
+console.log(ab);
+
+// ==>> var cho phép tái định nghĩa, còn let thì không
 
 const doSoi = 100;
-// doSoi = 50;  không thể tái định nghĩa
+// doSoi = 50;       ==>> không thể tái định nghĩa, thay đổi giá trị
 console.log(doSoi);
 
-// Khai báo đối tượng sử dụng let
+// Áp dụng với Khai báo đối tượng (let)
 let person = {
 	name: "John",
 	age: 30,
@@ -19,9 +30,9 @@ console.log(person);
 
 // Thử thay đổi giá trị của person
 person = 5;
-console.log(person); // bị thay đổi
+console.log(person); //    bị thay đổi => rủi ro bị mất giá trị gốc
 
-// Để tránh điều này xảy ra, thường sử dụng const để khai báo đối tượng
+// ===>> Để tránh điều này xảy ra, thường sử dụng const để khai báo đối tượng
 const person2 = {
 	name: "John",
 	age: 30,
@@ -30,12 +41,15 @@ console.log(person);
 
 // Thử thay đổi giá trị của person
 // person2 = 5;
-console.log(person2); // không bị thay đổi
+console.log(person2); //   không bị thay đổi (an toàn)
 
-// Và tất nhiên chúng ta vẫn thêm, sửa, xóa thuộc tính bình thường,
-// chỉ là 0 ghi đè được lại đối tượng đã khai báo
-person2.isAdmin = true; //Thêm mới thuộc tính cho person 2
-person2.name = "Tom"; //Sửa thuộc tính
-delete person2.age; //xóa thuộc tính
+// Và tất nhiên chúng ta vẫn thêm, sửa, xóa thuộc tính bình thường
+// Chỉ là 0 ghi đè được lại đối tượng đã khai báo
+person2.isAdmin = true; // Thêm mới thuộc tính
+person2.name = "Tom"; // Sửa thuộc tính
+delete person2.age; // xóa thuộc tính
 
 console.log(person2);
+
+// ==>> Sử dụng const trong khai báo đối tượng để không bị thay đổi giá trị gốc
+// Vẫn giữ nguyên được gốc là Object ban đầu thay vì bị đổi thành những giá trị khác
